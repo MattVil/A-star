18d7892736472da066135e82ee8761cbea276115
MattVil <mattvilain@gmail.com>
2017-02-15 12:44:04 +0100

    begin of PathFounder Class

---
+++ src/algorithm/PathFounder.java
@@ -0,0 +1,22 @@
+package algorithm;
+
+import java.util.LinkedList;
+
+import map.Map;
+
+/**
+ * 
+ * @author matthieu
+ *
+ */
+public class PathFounder {
+
+	private Map map;
+	//private LinkedList<E>
+	
+	public PathFounder(Map map){
+		this.map = map;
+	}
+	
+	//public Linked
+}
