d62c38e7f93bade8145df9fd2f71b9d6f446fa12
MattVil <mattvilain@gmail.com>
2017-02-15 18:46:08 +0100

    add previous node to Node class

--- src/map/Node.java
+++ src/map/Node.java
@@ -10,6 +10,7 @@ public class Node {
 	private Coordinate coord;
 	private int type;
 	private double value;
+	private Node previous;
 	
 	public Node(Coordinate coord, int type){
 		this.coord = coord;
@@ -41,6 +42,14 @@ public class Node {
 		this.value = value;
 	}
 	
+	public Node getPrevious() {
+		return previous;
+	}
+
+	public void setPrevious(Node previous) {
+		this.previous = previous;
+	}
+
 	public String toString(){
 		return type + " : " + coord.toString() + " - " + value; 
 	}
